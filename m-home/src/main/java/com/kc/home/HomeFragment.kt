5ec8546b6f4e15a135610754f0086a518f17169c
armStrong759 <3058587756@qq.com>
2021-05-19 12:22:27 +0800

    添加TitleBar

--- m-home/src/main/java/com/kc/home/HomeFragment.kt
+++ m-home/src/main/java/com/kc/home/HomeFragment.kt
@@ -1,9 +1,32 @@
 package com.kc.home
 
+import android.view.View
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.example.mykotlindemo.utils.toast
+import com.hjq.bar.OnTitleBarListener
 import com.kc.home.databinding.FragmentHomeBinding
 import com.kc.library.base.base.BaseMvvMFragment
 import com.kc.library.base.router.RouterFragmentPath
 
 @Route(path = RouterFragmentPath.Home.HOME_FRAGMENT)
-class HomeFragment : BaseMvvMFragment<FragmentHomeBinding,HomeViewModel>() { }
+class HomeFragment : BaseMvvMFragment<FragmentHomeBinding, HomeViewModel>() {
+
+    override fun onLoad(view: View) {
+        super.onLoad(view)
+        dataBinding.toolBar.setOnTitleBarListener(object : OnTitleBarListener {
+            override fun onLeftClick(v: View) {
+                //左边
+//                activity?.finish()
+            }
+
+            override fun onTitleClick(v: View) {
+                //中间
+            }
+
+            override fun onRightClick(v: View) {
+                //右边
+                toast("搜索")
+            }
+        })
+    }
+}
