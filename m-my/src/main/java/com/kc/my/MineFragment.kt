ba283d0019336d4f779d562aa0cc3e33b447d655
armStrong759 <3058587756@qq.com>
2021-05-24 11:29:14 +0800

    闪屏页图片更换+我的系列跳转其他app

--- m-my/src/main/java/com/kc/my/MineFragment.kt
+++ m-my/src/main/java/com/kc/my/MineFragment.kt
@@ -1,5 +1,7 @@
 package com.kc.my
 
+import android.content.ComponentName
+import android.content.Intent
 import android.view.View
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.google.android.material.appbar.AppBarLayout
@@ -12,12 +14,59 @@ class MineFragment : BaseMvvMFragment<FragmentMineBinding, MineViewModel>() {
     override fun isFitsSystemWindow(): Boolean = false
     override fun onLoad(view: View) {
         super.onLoad(view)
-        //滑动监听事件
+        //appbarlayout滑动监听事件
         dataBinding.appBarLayout.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
             if (verticalOffset < -500) {
                 dataBinding.endScrollContentView.visibility = View.VISIBLE
             } else {
                 dataBinding.endScrollContentView.visibility = View.INVISIBLE
+            }
+        })
+
+        viewModel.goToJavaDemoLiveData.observe(this, {
+            if (it) {
+                //第一种方式：通过包名+指定Activity路径跳转
+                val intent = Intent(Intent.ACTION_MAIN)
+                val componentName = ComponentName(
+                    "com.example.mydemo",
+                    "com.example.mydemo.activity.MainActivity"
+                )
+                intent.component = componentName
+                intent.putExtra("", "") //这里通过Intent传值。
+                startActivity(intent)
+//                第二种方式：通过包名跳转到另一个app的启动页
+//                val intent: Intent = this.getPackageManager().getLaunchIntentForPackage("kuyu.com.xxxx")
+//                if (intent != null) {
+//                    intent.putExtra("type", "110")
+//                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+//                    startActivity(intent)
+//                }
+            }
+        })
+
+        viewModel.goToKotlinDemoLiveData.observe(this, {
+            if (it) {
+                val intent = Intent(Intent.ACTION_MAIN)
+                val componentName = ComponentName(
+                    "com.example.mykotlindemo",
+                    "com.example.mykotlindemo.main.MainActivity"
+                )
+                intent.component = componentName
+                intent.putExtra("", "")
+                startActivity(intent)
+            }
+        })
+
+        viewModel.goToDiyDemoLiveData.observe(this, {
+            if (it) {
+                val intent = Intent(Intent.ACTION_MAIN)
+                val componentName = ComponentName(
+                    "com.example.diyview",
+                    "com.example.diyview.main.MainActivity"
+                )
+                intent.component = componentName
+                intent.putExtra("", "")
+                startActivity(intent)
             }
         })
     }
--- m-my/src/main/java/com/kc/my/MineViewModel.kt
+++ m-my/src/main/java/com/kc/my/MineViewModel.kt
@@ -1,7 +1,10 @@
 package com.kc.my
 
 import android.app.Application
+import android.content.ComponentName
+import android.content.Intent
 import androidx.databinding.ObservableArrayList
+import androidx.lifecycle.MutableLiveData
 import com.alibaba.android.arouter.launcher.ARouter
 import com.kc.library.base.base.BaseViewModel
 import com.kc.library.base.router.RouterActivityPath
@@ -27,12 +30,26 @@ class MineViewModel(application: Application) : BaseViewModel(application) {
         items.add("其他")
     }
 
+    val goToJavaDemoLiveData = MutableLiveData(false)
+    val goToKotlinDemoLiveData = MutableLiveData(false)
+    val goToDiyDemoLiveData = MutableLiveData(false)
+
     fun onClickItem(item:String){
         when(item){
             "我的收藏"->{
                 ARouter.getInstance().build(RouterActivityPath.Collect.COLLECT_ACTIVITY).navigation()
             }
             "我的文章"->{}
+            "我的Java系列"->{
+                goToJavaDemoLiveData.postValue(true)
+            }
+            "我的Kotlin系列"->{
+                goToKotlinDemoLiveData.postValue(true)
+            }
+            "我的DiyView系列"->{
+                goToDiyDemoLiveData.postValue(true)
+            }
+            "我的试炼场"->{}
             "其他"->{}
         }
     }
